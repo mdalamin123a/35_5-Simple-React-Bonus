4ebaf5b2ceeaf06b457ba323d3c92126cb5b988a
mdalamin123a <md.alamindev90@gmail.com>
2024-10-26 12:52:19 +0600

    35_5-3-Build-Team-And-How-React-Work

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import User from './components/User/User';
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [team, setTeam] = useState([]);
 
   useEffect(()=>{
     fetch('https://randomuser.me/api/?results=15')
@@ -12,17 +13,17 @@ function App() {
   },[]);
 
   const addMember = (name) => {
-    console.log("member added", name);
+    setTeam([...team, name]);
   }
-
-  // const add = (x, y) => x + y;
-  // const addWrapper = () => add(3, 5);
-  // const total = addWrapper;
-  // console.log(total);
 
   return (
     <div>
       <h1>Team Builder</h1>
+      <ul>
+        {
+          team.map((m, inx) => <li key={inx}>{m}</li>)
+        }
+      </ul>
       {
         users.map(user => <User user={user} addMember={addMember}></User>)
       }
